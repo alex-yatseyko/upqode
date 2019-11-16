791c6a6ad27bdac1877a8f3550639b0c8d2441ac
System Administrator <alex.yatseiko@gmail.com>
2019-11-16 14:28:55 +0200

    slider-2

--- src/5_slider/js/script.js
+++ src/5_slider/js/script.js
@@ -1,31 +1,47 @@
 window.onload = function(){
 
-    var btn_prev = document.querySelector('#gallery .buttons .prev');
-    var btn_next = document.querySelector('#gallery .buttons .next');
+    const btn_prev = document.querySelector('#gallery .buttons .prev');
+    const btn_next = document.querySelector('#gallery .buttons .next');
 
-    var images = document.querySelectorAll('#gallery .photos img');
-    var i = 0;
+    const images = document.querySelectorAll('#gallery .photos img');
+    let i = 0;
+
+    const button_die = () => {
+        if(i == 0) {
+            btn_prev.setAttribute("disabled", "true");
+            btn_prev.onclick = () => {
+                return;
+            }
+        }
+    }
 
     btn_prev.onclick = function(){
+        button_die()
+
          images[i].classList.remove('showed');
          i--;
          
          if(i < 0){
-             i = images.length - 1;
+             // i = images.length - 1;
          }
          
          images[i].classList.add('showed');
+
+         
     }
 
     btn_next.onclick = function(){
+        button_die()
          images[i].classList.remove('showed');
          i++;
          
          if(i >= images.length){
-             i = 0;
+            btn_next.disabled
          }
-         
+        
          images[i].classList.add('showed');
+
+         
     }
 
 }
